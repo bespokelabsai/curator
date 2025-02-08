fd735da72eaf8aeb510574f7734cba9aab01ecf0
Ryan Marten <ryanmarten2000@gmail.com>
2025-02-08 15:52:20 -0800

    update comments

--- examples/providers/openrouter_reasoning_online.py
+++ examples/providers/openrouter_reasoning_online.py
@@ -27,24 +27,24 @@ class Reasoner(curator.LLM):
 
 llm = Reasoner(
     model_name="deepseek/deepseek-r1",  # https://openrouter.ai/deepseek/deepseek-r1
-    backend="openai",
+    backend="openai",  # Generic openai API compatible backend
     generation_params={
         "include_reasoning": True,  # https://openrouter.ai/announcements/reasoning-tokens-for-thinking-models
-        "max_tokens": 40000,  # Matching DeepSeek's official API has 32k CoT and 8k max answer tokens
+        "max_tokens": 40000,  # Matching DeepSeek's official API that has 32k CoT and 8k max answer tokens
         "provider": {  # https://openrouter.ai/docs/provider-routing
             "order": ["Fireworks", "Kluster"],  # https://openrouter.ai/deepseek/deepseek-r1/providers
             "allow_fallbacks": False,  # Strictly require providers given
-            "sort": "throughput",
-            "require_parameters": True,
+            "sort": "throughput",  # Use the highest throughput provider
+            "require_parameters": True,  # Require include_reasoning and max_tokens
         },
     },
     backend_params={
         "base_url": "https://openrouter.ai/api/v1",
         "api_key": os.environ.get("OPENROUTER_API_KEY"),
-        "max_retries": 5,
-        "max_requests_per_minute": 20,
-        "max_tokens_per_minute": 10000000000000,
-        "request_timeout": 60 * 30,
+        "max_retries": 5,  # Some requests fail due to provider rate limiting or other issues
+        "max_requests_per_minute": 20,  # Based on rate limiting of the providers
+        "max_tokens_per_minute": 10000000000000,  # Set high to only rate limit on requests
+        "request_timeout": 60 * 30,  # Reasoning models have lots of tokens and can take a while to respond
     },
 )
 
