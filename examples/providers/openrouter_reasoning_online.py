9502d2a4342026ff37dd383c20bf9156d881d447
Ryan Marten <ryanmarten2000@gmail.com>
2025-02-08 15:46:29 -0800

    update example

---
+++ examples/providers/openrouter_reasoning_online.py
@@ -0,0 +1,55 @@
+import os
+
+from datasets import Dataset
+
+from bespokelabs import curator
+
+
+class Reasoner(curator.LLM):
+    """Curator class for processing Numina dataset."""
+
+    return_completions_object = True
+
+    def prompt(self, input):
+        """Create a prompt for the LLM to reason about the problem."""
+        return [{"role": "user", "content": input["problem"]}]
+
+    def parse(self, input, response):
+        """Parse the LLM response to extract reasoning and solution."""
+        return [
+            {
+                "problem": input["problem"],
+                "deepseek_reasoning": response["choices"][0]["message"]["reasoning"],
+                "deepseek_solution": response["choices"][0]["message"]["content"],
+            }
+        ]
+
+
+llm = Reasoner(
+    model_name="deepseek/deepseek-r1",  # https://openrouter.ai/deepseek/deepseek-r1
+    backend="openai",
+    generation_params={
+        "include_reasoning": True,  # https://openrouter.ai/announcements/reasoning-tokens-for-thinking-models
+        "max_tokens": 40000,  # Matching DeepSeek's official API has 32k CoT and 8k max answer tokens
+        "provider": {  # https://openrouter.ai/docs/provider-routing
+            "order": ["Fireworks", "Kluster"],  # https://openrouter.ai/deepseek/deepseek-r1/providers
+            "allow_fallbacks": False,  # Strictly require providers given
+            "sort": "throughput",
+            "require_parameters": True,
+        },
+    },
+    backend_params={
+        "base_url": "https://openrouter.ai/api/v1",
+        "api_key": os.environ.get("OPENROUTER_API_KEY"),
+        "max_retries": 5,
+        "max_requests_per_minute": 20,
+        "max_tokens_per_minute": 10000000000000,
+        "request_timeout": 60 * 30,
+    },
+)
+
+dataset = Dataset.from_dict({"problem": ["Find the sum of all integer bases $b > 9$ for which $17_b$ is a divisor of $97_b$."]})  # The answer is 70 (AIME25)
+
+response = llm(dataset)
+print("REASONING: ", response["deepseek_reasoning"])
+print("SOLUTION: ", response["deepseek_solution"])
