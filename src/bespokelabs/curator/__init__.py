a4907fb4eb3ff69edb1204be99abc0319eae4a9e
TheDude <kartik4949@gmail.com>
2025-07-10 18:20:43 +0530

    ref: add version tag in lib

--- src/bespokelabs/curator/__init__.py
+++ src/bespokelabs/curator/__init__.py
@@ -1,10 +1,17 @@
 """BespokeLabs Curator."""
 
+import importlib.metadata
+
+try:
+    __version__ = importlib.metadata.version("bespokelabs-curator")
+except importlib.metadata.PackageNotFoundError:
+    __version__ = "unknown"
+
 from .code_executor.code_executor import CodeExecutor
 from .llm.llm import LLM
 from .types import prompt as types
 from .utils import load_dataset, push_to_viewer
 
-__all__ = ["LLM", "CodeExecutor", "types", "push_to_viewer", "load_dataset"]
+__all__ = ["LLM", "CodeExecutor", "types", "push_to_viewer", "load_dataset", "__version__"]
 
 from .log import _CONSOLE  # noqa: F401
